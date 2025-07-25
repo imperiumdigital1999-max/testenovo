b51283646061fc7cb20bc7397e854cba182a34dd
imperiumdigital1999-max <imperium.digital1999@gmail.com>
2025-07-25 00:01:14 -0300

    Fix useEffect import error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate, Outlet, useLocation } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import Layout from '@/components/Layout';
